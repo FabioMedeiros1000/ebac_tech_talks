39357ca12f04161f84fddbf1edf5c911feee96c5
FabioMedeiros1000 <fabio.leandro.medeiros@gmail.com>
2024-07-22 14:44:44 -0300

    Mudando a data do evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Jul 22, 2024 19:00:00")
+const dataDoEvento = new Date("Dec 24, 2024 19:00:00")
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function(){
